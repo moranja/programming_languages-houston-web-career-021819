d3287b1a49860b9c3aab6821864d7156ae2327e2
Adam Moran <jeffadammoran@gmail.com>
2019-02-12 17:46:34 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  reformatted_hash = {}
+  languages.each do |type, language|
+    language.each do |lang, info|
+      if reformatted_hash[lang] == NIL
+        reformatted_hash[lang] = {}
+      end
+    end
+  end
+  
+  new_hash = {}
+  #new_hash[:test] ="test"
+    
+  languages.each do |type, language|
+    language_type = type
+    language.each do |lang, info|
+      reformatted_hash.each do |lang_r, info_r|
+        binding.pry
+        if lang_r == lang
+          info_r.each do |key, value|
+            new_hash = {key => value, :style => language_type}
+            new_hash[key] = value
+            new_hash[:style] = language_type
+            reformatted_hash[lang_r] = new_hash
+            binding.pry
+          end
+        end
+      end
+    end
+  end
+  
+      
+  return reformatted_hash
 end
+
+
+
+
+
+
