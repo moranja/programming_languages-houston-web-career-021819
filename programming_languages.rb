bcf2011fa5155d8f4ba154032646d1fa3b2e3f62
Adam Moran <jeffadammoran@gmail.com>
2019-02-12 18:00:11 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,32 @@
 def reformat_languages(languages)
-  # your code here
+  reformatted_hash = {}
+  languages.each do |type, language|
+    language.each do |lang, info|
+      if reformatted_hash[lang] == NIL
+        reformatted_hash[lang] = {}
+      end
+    end
+  end
+  
+  languages.each do |type, language|
+    language.each do |lang, info|
+      reformatted_hash.each do |lang_r, info_r|
+        if lang_r == lang
+          if info_r == {}
+            new_array = []
+            info.each do |key, value|
+              new_array << type
+              new_hash = {key => value, :style => new_array}
+              reformatted_hash[lang_r] = new_hash
+            end
+          else
+            info.each do |key, value|
+              reformatted_hash[lang_r][:style] << type
+            end
+          end
+        end
+      end
+    end
+  end
+  return reformatted_hash
 end
